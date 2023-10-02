ab84453a54454e63cfeda347275d10bef87218fe
Ricardo Borrego <101906700+ricardobm255@users.noreply.github.com>
2023-10-02 20:04:44 +0000

    Cambio de bombre del archivo

--- pleasuretv_bot.py
+++ pleasuretv_bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # Instantiate bot
-bot = telebot.TeleBot('5900264461:AAHxo563dyySXhgq3_J06OFvb_v_WMfQlCQ')
+bot = telebot.TeleBot('6666929357:AAFk2Wd7K13VPV8P-KB8X1A0Uu9pa46AENc')
 word = pyfiglet.figlet_format('SERVER IS ONLINE')
 print(word)
 
@@ -20,7 +20,7 @@ def handle_start(message):
     '''Funcion que da inicio al bot'''
     user_first_name = str(message.chat.first_name)
     # current_time = datetime.datetime.now().time()
-    welcome_message = f"Bienvenido {user_first_name}! 🎉\nel título de la película que desea buscar 🔍"
+    welcome_message = f"🎉Bienvenido {user_first_name}! \n🔍Escribe el título de la película que deseas buscar..."
 
     logger.info("El usuario %s ha iniciado el bot.", user_first_name)
 
@@ -30,10 +30,12 @@ def handle_start(message):
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     '''Funcion para buscar peliculas por el titulo y devuelve la informacion'''
+    user_first_name = str(message.chat.first_name)
     user_input = message.text
+    print(f"El usuario {user_first_name} esta buscando '{user_input}'")
     bot.send_message(message.chat.id, "Espere por favor, buscando...!")
     try:
-        # Set the API key (sign up at https://www.themoviedb.org/ to obtain your own API key)
+        # Set the API key (https://www.themoviedb.org/ )
         api_key = "efcfe9f6ebd1de85441306f3a65d6159"
 
         # Set the base URL of the API
@@ -50,7 +52,7 @@ def handle_message(message):
             "page": 1  # Set the page number
         }
 
-        # Make the API request
+        # Make the API request Spanish
         response = requests.get(base_url + endpoint,
                                 params=params, timeout=10.001)
         # Raise an exception if an error occurs with the API request
@@ -68,15 +70,34 @@ def handle_message(message):
             first_movie = results[0]
 
             # Extract the desired data
-            poster_path = first_movie["poster_path"]
+            # poster_path = first_movie["poster_path"]
             original_title = first_movie["original_title"]
             title = first_movie["title"]
             overview = first_movie["overview"]
             original_language = first_movie["original_language"]
             movie_id = first_movie["id"]
 
-            num_caract = 400
+            # Formatear la Sinopsis(overview) para q el mensaje no supere los 1024 characters
+            num_caract = 600
             substring = overview[:num_caract]
+
+            # Formatear el lenguaje original de la pelicula
+            def get_language_name(original_language):
+                language_names = {
+                    "en": "Inglés",
+                    "es": "Español",
+                    "fr": "Francés",
+                    "pt": "Portugués",
+                    "it": "Italiano",
+                    "ja": "Japonés",
+                    "ko": "Coreano",
+                    "ru": "Ruso",
+                    "uk": "Ucraniano"
+                }
+
+                return language_names.get(original_language, "Unknown")
+
+            language_name = get_language_name(original_language)
 
             # Get the details of the movie
             movie_details_endpoint = f"movie/{movie_id}"
@@ -96,9 +117,30 @@ def handle_message(message):
             # Get the IMDb_ID of the movie
             imdb_id = details_data.get("imdb_id")
 
+            # Extract torrents links
+            def get_torrent_links(imdb_id):
+                url = f"https://yts.mx/api/v2/list_movies.json?query_term={imdb_id}"
+                response = requests.get(url, timeout=10.001)
+                data = response.json()
+
+                if "data" in data and "movies" in data["data"]:
+                    movies = data["data"]["movies"]
+                    if movies:
+                        movie = movies[0]
+                        torrents = movie["torrents"]
+                        links_with_quality = [
+                            (torrent["url"], torrent["quality"]) for torrent in torrents]
+                        return links_with_quality
+                return []
+
+            torrent_links = get_torrent_links(imdb_id)
+
+            message_torrent ="Torrent Links:\n"
+            for link, quality in torrent_links:
+                message_torrent += f"🥇 Quality: {quality}\n🔗 Torrent Link: {link}"'\n''\n'
             # Make the HTTP GET request to the API
             response = requests.get(
-                f"https://yts.mx/api/v2/movie_details.json?with_images=false&with_cast=false&imdb_id={imdb_id}", timeout=10.001)
+                f"https://yts.mx/api/v2/movie_details.json?with_images=false&with_cast=true&imdb_id={imdb_id}", timeout=10.001)
 
             # Check if the request was successful (status code 200)
             response.raise_for_status()
@@ -109,9 +151,47 @@ def handle_message(message):
             genre_names = [genre["name"] for genre in genres]
             formatted_genres = ' #'.join(genre_names)
 
+            # Extraer el valor de "runtime" de los datos de la primera pelicula
+            runtime_in_minutes = movie_details["runtime"]
+
+            # Convertir "runtaime" en horas y minutos
+            hours, minutes = divmod(runtime_in_minutes, 60)
+
+            def format_hours_minutes(hours, minutes):
+
+                formatted_time = f"{hours}h {minutes}m"
+                return formatted_time
+
+            formatted_time = format_hours_minutes(hours, minutes)
+
+            # Set the query parameters for search the original image in English
+            params_en = {
+                "api_key": api_key,
+                "query": user_input,
+                "language": "en",  # Set language to English
+                "page": 1  # Set the page number
+            }
+
+            # Make the API request
+            response = requests.get(base_url + endpoint,
+                                    params=params_en, timeout=10.001)
+            # Raise an exception if an error occurs with the API request English
+            response.raise_for_status()
+
+            # Get the JSON data from the response English
+            data = response.json()
+
+            # Extract the list of movies from the response English
+            results = data["results"]
+
+            # Get the first movie English
+            first_movie_english = results[0]
+
+            poster_path = first_movie_english["poster_path"]
+
             # Evaluate what if the best quality image
             # Add the desired qualities in the order you want to check
-            qualities = ["w500"]
+            qualities = ["w500"]  # Incluide "w780", "original"
 
             # Construct the API endpoint URL for fetching the image details
             api_url = f"https://api.themoviedb.org/3/configuration?api_key={api_key}"
@@ -147,24 +227,20 @@ def handle_message(message):
                         best_quality_link = image_url
                         best_quality_width = width
 
-            # Print the best quality link (if available)
-            if best_quality_link:
-                print("The link with the highest available quality:",
-                      best_quality_link)
-            else:
-                print("No available image links found.")
-
             try:
                 bot.send_photo(message.chat.id, best_quality_link, f"📺 Título: {original_title}" '\n'
                                f"🖥 Otro Título: {title}" '\n'
                                f"📆 Año: {movie_details['year']}" '\n'
-                               f"🎙 Audio: {original_language}" '\n'
-                               f"🕰 Duración: {movie_details['runtime']}" '\n'
+                               f"🎙 Audio: {language_name}" '\n'
+                               f"🕰 Duración: {formatted_time}" '\n'
                                f"🌟 IMDb: {movie_details['rating']}" '\n'
+                               f"🎟 Reparto: {', '.join([cast['name']for cast in movie_details['cast'][:4]])}"'\n'
                                f"🔣 Géneros: #{formatted_genres}"'\n''\n'
-                               f"🏷 Sinopsis: {substring}..."'\n''\n'
+                               f"🏷 Sinopsis: {substring}"'\n''\n'
                                f"▶️ DESCARGAR ▶️"'\n''\n'
                                f"⚠️ Subtítulos en los comentarios ⚠️")
+                bot.send_message(message.chat.id, message_torrent)
+                print("Busqueda completada!!!")
             except telebot.apihelper.ApiException as err:
                 logger.error('BadRequest error: %s', err)
                 bot.send_message(
