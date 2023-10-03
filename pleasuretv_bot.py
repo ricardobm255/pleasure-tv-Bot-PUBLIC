825ec622ac01f6128571312169111bdec6d1b4a2
Ricardo Borrego <101906700+ricardobm255@users.noreply.github.com>
2023-10-02 21:44:28 -0400

    Corregido error de sintaxis Corregido error en la solicitud de imagenes

--- pleasuretv_bot.py
+++ pleasuretv_bot.py
@@ -1,9 +1,10 @@
 '''Import all libraries necessaries'''
 import logging
+import json
 import telebot
 import pyfiglet
 import requests
-import json
+
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -37,7 +38,7 @@ def handle_message(message):
     bot.send_message(message.chat.id, "Espere por favor, buscando...!")
     try:
         # Set the API key (https://www.themoviedb.org/ )
-        api_key = "efcfe9f6ebd1de85441306f3a65d6159"
+        api_key = "7c35175bd929415d243d6bf2e1a6bb02"
 
         # Set the base URL of the API
         base_url = "https://api.themoviedb.org/3/"
@@ -52,7 +53,6 @@ def handle_message(message):
             "language": "es",  # Set language to Spanish
             "page": 1  # Set the page number
         }
-        
         try:
             # Make the API request Spanish
             response = requests.get(base_url + endpoint,
@@ -62,99 +62,86 @@ def handle_message(message):
 
             # Get the JSON data from the response
             data = json.loads(response.text)
-        except requests.exceptions.RequestException as e:
-            print(f"Error de solicitud: {e}")
-        except json.JSONDecodeError as e:
-            print(f"Error al decodificar JSON: {e}")
+            print(data)
 
-        # Extract the list of movies from the response
-        results = data["results"]
+            # Extract the list of movies from the response
+            results = data["results"]
 
-        if results:
+            if results:
+                # Get the first movie
+                first_movie = results[0]
 
-            # Get the first movie
-            first_movie = results[0]
+                # Extract the desired data
+                # poster_path = first_movie["poster_path"]
+                original_title = first_movie["original_title"]
+                title = first_movie["title"]
+                overview = first_movie["overview"]
+                original_language = first_movie["original_language"]
+                movie_id = first_movie["id"]
 
-            # Extract the desired data
-            # poster_path = first_movie["poster_path"]
-            original_title = first_movie["original_title"]
-            title = first_movie["title"]
-            overview = first_movie["overview"]
-            original_language = first_movie["original_language"]
-            movie_id = first_movie["id"]
+                # Format overview for not superate limit at 1024 characters
+                num_caract = 600
+                substring = overview[:num_caract]
 
-            # Format overview for not superate limit at 1024 characters
-            num_caract = 600
-            substring = overview[:num_caract]
+                # Format original lenguage to spanish
+                def get_language_name(original_language):
+                    language_names = {
+                        "en": "Inglés",
+                        "es": "Español",
+                        "fr": "Francés",
+                        "pt": "Portugués",
+                        "it": "Italiano",
+                        "ja": "Japonés",
+                        "ko": "Coreano",
+                        "ru": "Ruso",
+                        "uk": "Ucraniano"
+                    }
 
-            # Format original lenguage to spanish
-            def get_language_name(original_language):
-                language_names = {
-                    "en": "Inglés",
-                    "es": "Español",
-                    "fr": "Francés",
-                    "pt": "Portugués",
-                    "it": "Italiano",
-                    "ja": "Japonés",
-                    "ko": "Coreano",
-                    "ru": "Ruso",
-                    "uk": "Ucraniano"
+                    return language_names.get(original_language, "Unknown")
+
+                language_name = get_language_name(original_language)
+
+                # Get details of the movie
+                movie_details_endpoint = f"movie/{movie_id}"
+                details_params = {
+                    "api_key": api_key,
+                    "language": "es"  # Set language to Spanish
                 }
-
-                return language_names.get(original_language, "Unknown")
-
-            language_name = get_language_name(original_language)
-
-            # Get details of the movie
-            movie_details_endpoint = f"movie/{movie_id}"
-            details_params = {
-                "api_key": api_key,
-                "language": "es"  # Set language to Spanish
-            }
-            try:
                 details_response = requests.get(base_url + movie_details_endpoint,
                                                 params=details_params, timeout=10.001)
                 details_response.raise_for_status()
                 details_data = json.loads(details_response.text)
-            except requests.exceptions.RequestException as e:
-                print(f"Error de solicitud: {e}")
-            except json.JSONDecodeError as e:
-                print(f"Error al decodificar JSON: {e}")
 
-            # Get the genres of the movie
-            genres = details_data.get("genres")
+                # Get the genres of the movie
+                genres = details_data.get("genres")
 
-            # Get the IMDb_ID of the movie
-            imdb_id = details_data.get("imdb_id")
+                # Get the IMDb_ID of the movie
+                imdb_id = details_data.get("imdb_id")
 
-            # Extract torrents links
-            def get_torrent_links(imdb_id):
-                url = f"https://yts.mx/api/v2/list_movies.json?query_term={imdb_id}"
-                try:
+                # Extract torrents links
+                def get_torrent_links(imdb_id):
+                    url = f"https://yts.mx/api/v2/list_movies.json?query_term={imdb_id}"
+
                     response = requests.get(url, timeout=10.001)
                     data = json.loads(response.text)
-                except requests.exceptions.RequestException as e:
-                    print(f"Error de solicitud: {e}")
-                except json.JSONDecodeError as e:
-                    print(f"Error al decodificar JSON: {e}")
 
-                if "data" in data and "movies" in data["data"]:
-                    movies = data["data"]["movies"]
-                    if movies:
-                        movie = movies[0]
-                        torrents = movie["torrents"]
-                        links_with_quality = [
-                            (torrent["url"], torrent["quality"]) for torrent in torrents]
-                        return links_with_quality
-                return []
+                    if "data" in data and "movies" in data["data"]:
+                        movies = data["data"]["movies"]
+                        if movies:
+                            movie = movies[0]
+                            torrents = movie["torrents"]
+                            links_with_quality = [
+                                (torrent["url"], torrent["quality"]) for torrent in torrents]
+                            return links_with_quality
+                    return []
 
-            torrent_links = get_torrent_links(imdb_id)
+                torrent_links = get_torrent_links(imdb_id)
 
-            message_torrent ="Torrent Links:\n"
-            for link, quality in torrent_links:
-                message_torrent += f"🥇 Calidad: {quality}\n🔗 Torrent Link: {link}"'\n''\n'
-            # Make the HTTP GET request to the API
-            try:
+                message_torrent = "Torrent Links:\n"
+                for link, quality in torrent_links:
+                    message_torrent += f"🥇 Calidad: {quality}\n🔗 Torrent Link: {link}"'\n''\n'
+                # Make the HTTP GET request to the API
+
                 response = requests.get(
                     f"https://yts.mx/api/v2/movie_details.json?with_images=false&with_cast=true&imdb_id={imdb_id}", timeout=10.001)
 
@@ -162,38 +149,32 @@ def handle_message(message):
                 response.raise_for_status()
                 # Get the JSON data from the response
                 movie_details = json.loads(response.text)["data"]["movie"]
-            except requests.exceptions.RequestException as e:
-                print(f"Error de solicitud: {e}")
-            except json.JSONDecodeError as e:
-                print(f"Error al decodificar JSON: {e}")
+                # Extract all genre names
+                genre_names = [genre["name"] for genre in genres]
+                formatted_genres = ' #'.join(genre_names)
 
-            # Extract all genre names
-            genre_names = [genre["name"] for genre in genres]
-            formatted_genres = ' #'.join(genre_names)
+                # Extract the runtime value (this return "runtime" in minutes)
+                runtime_in_minutes = movie_details["runtime"]
 
-            # Extract the runtime value (this return "runtime" in minutes)
-            runtime_in_minutes = movie_details["runtime"]
+                # Convert runtime (minutes) in hours and minutes
+                hours, minutes = divmod(runtime_in_minutes, 60)
 
-            # Convert runtime (minutes) in hours and minutes
-            hours, minutes = divmod(runtime_in_minutes, 60)
+                def format_hours_minutes(hours, minutes):
 
-            def format_hours_minutes(hours, minutes):
+                    formatted_time = f"{hours}h {minutes}m"
+                    return formatted_time
 
-                formatted_time = f"{hours}h {minutes}m"
-                return formatted_time
+                formatted_time = format_hours_minutes(hours, minutes)
 
-            formatted_time = format_hours_minutes(hours, minutes)
+                # Set the query parameters for search the original image in English
+                params_en = {
+                    "api_key": api_key,
+                    "query": user_input,
+                    "language": "en",  # Set language to English
+                    "page": 1  # Set the page number
+                }
 
-            # Set the query parameters for search the original image in English
-            params_en = {
-                "api_key": api_key,
-                "query": user_input,
-                "language": "en",  # Set language to English
-                "page": 1  # Set the page number
-            }
-
-            # Make the API request
-            try:
+                # Make the API request
                 response = requests.get(base_url + endpoint,
                                         params=params_en, timeout=10.001)
                 # Raise an exception if an error occurs with the API request in English
@@ -201,87 +182,83 @@ def handle_message(message):
 
                 # Get the JSON data from the response English
                 data = json.loads(response.text)
-            except requests.exceptions.RequestException as e:
-                print(f"Error de solicitud: {e}")
-            except json.JSONDecodeError as e:
-                print(f"Error al decodificar JSON: {e}")
 
-            # Extract the list of movies from the response English
-            results = data["results"]
+                # Extract the list of movies from the response English
+                results = data["results"]
 
-            # Get the results of the first movie in English
-            first_movie_english = results[0]
+                # Get the results of the first movie in English
+                first_movie_english = results[0]
 
-            poster_path = first_movie_english["poster_path"]
+                poster_path = first_movie_english["poster_path"]
 
-            # Evaluate what if the best quality image
-            # Add the desired qualities in the order you want to check
-            qualities = ["w500", "w780", "original"] 
+                # Evaluate what if the best quality image
+                # Add the desired qualities in the order you want to check
+                qualities = ["w500"]#, "w780", "original"
 
-            # Construct the API endpoint URL for fetching the image details
-            api_url = f"https://api.themoviedb.org/3/configuration?api_key={api_key}"
+                # Construct the API endpoint URL for fetching the image details
+                api_url = f"https://api.themoviedb.org/3/configuration?api_key={api_key}"
 
-            # Make a GET request to fetch the configuration details
-            try:
+                # Make a GET request to fetch the configuration details
                 response = requests.get(api_url, timeout=10.001)
                 if response.status_code != 200:
                     print("Failed to fetch configuration details")
                     exit()
 
                 # Extract the base URL for images from the response
-                base_url = json.loads(response.text)["images"]["secure_base_url"]
-            except requests.exceptions.RequestException as e:
-                print(f"Error de solicitud: {e}")
-            except json.JSONDecodeError as e:
-                print(f"Error al decodificar JSON: {e}")
+                base_url = json.loads(response.text)[
+                    "images"]["secure_base_url"]
 
-            # Initialize the best quality link and its width as None
-            best_quality_link = None
-            best_quality_width = 0
+                # Initialize the best quality link and its width as None
+                best_quality_link = None
+                best_quality_width = 0
 
-            # Iterate over the qualities to check the availability of each quality
-            for quality in qualities:
-                # Construct the complete image URL for the current quality
-                image_url = f"{base_url}{quality}{poster_path}"
+                # Iterate over the qualities to check the availability of each quality
+                for quality in qualities:
+                    # Construct the complete image URL for the current quality
+                    image_url = f"{base_url}{quality}{poster_path}"
 
-                # Make a GET request to the image URL
-                response = requests.get(image_url, timeout=10.001)
+                    # Make a GET request to the image URL
+                    response = requests.get(image_url, timeout=10.001)
 
-                # Check if the request was successful (status code 200) and the content type is an image
-                if response.status_code == 200 and response.headers.get("content-type", "").startswith("image/"):
-                    # Extract the width of the available image
-                    width = int(response.headers["content-length"])
+                    # Check if the request was successful (status code 200) and the content type is an image
+                    if response.status_code == 200 and response.headers.get("content-type", "").startswith("image/"):
+                        # Extract the width of the available image
+                        width = int(response.headers["content-length"])
 
-                    # Update the best quality link if the current quality is higher than the previous best
-                    if width > best_quality_width:
-                        best_quality_link = image_url
-                        best_quality_width = width
+                        # Update the best quality link if the current quality is higher than the previous best
+                        if width > best_quality_width:
+                            best_quality_link = image_url
+                            best_quality_width = width
 
-            try:
-                bot.send_photo(message.chat.id, best_quality_link, f"📺 Título: {original_title}" '\n'
-                               f"🖥 Otro Título: {title}" '\n'
-                               f"📆 Año: {movie_details['year']}" '\n'
-                               f"🎙 Audio: {language_name}" '\n'
-                               f"🕰 Duración: {formatted_time}" '\n'
-                               f"🌟 IMDb: {movie_details['rating']}" '\n'
-                               f"🎟 Reparto: {', '.join([cast['name']for cast in movie_details['cast'][:4]])}"'\n'
-                               f"🔣 Géneros: #{formatted_genres}"'\n''\n'
-                               f"🏷 Sinopsis: {substring}"'\n''\n'
-                               f"▶️ DESCARGAR ▶️"'\n''\n'
-                               f"⚠️ Subtítulos en los comentarios ⚠️")
-                bot.send_message(message.chat.id, message_torrent)
-                print("Busqueda completada!!!") # Print in console
-            except telebot.apihelper.ApiException as err:
-                logger.error('BadRequest error: %s', err)
+                try:
+                    bot.send_photo(message.chat.id, best_quality_link, f"📺 Título: {original_title}" '\n'
+                                   f"🖥 Otro Título: {title}" '\n'
+                                   f"📆 Año: {movie_details['year']}" '\n'
+                                   f"🎙 Audio: {language_name}" '\n'
+                                   f"🕰 Duración: {formatted_time}" '\n'
+                                   f"🌟 IMDb: {movie_details['rating']}" '\n'
+                                   f"🎟 Reparto: {', '.join([cast['name']for cast in movie_details['cast'][:4]])}"'\n'
+                                   f"🔣 Géneros: #{formatted_genres}"'\n''\n'
+                                   f"🏷 Sinopsis: {substring}"'\n''\n'
+                                   f"▶️ DESCARGAR ▶️"'\n''\n'
+                                   f"⚠️ Subtítulos en los comentarios ⚠️")
+                    bot.send_message(message.chat.id, message_torrent)
+                    print("Busqueda completada!!!")  # Print in console
+                except telebot.apihelper.ApiException as err:
+                    logger.error('BadRequest error: %s', err)
+                    bot.send_message(
+                        message.chat.id, 'Se produjo un error al procesar su solicitud. Por favor, inténtelo de nuevo.')
+            else:
                 bot.send_message(
-                    message.chat.id, 'Se produjo un error al procesar su solicitud. Por favor, inténtelo de nuevo.')
-        else:
-            bot.send_message(
-                message.chat.id, "No se encontraron resultados para su consulta.")
-            # If the request was not successful, print the status code
-            logger.error(
-                "No se encontraron resultados para la consulta del usuario.")
-            logger.error("Código de estado: %s", response.status_code)
+                    message.chat.id, "No se encontraron resultados para su consulta.")
+                # If the request was not successful, print the status code
+                logger.error(
+                    "No se encontraron resultados para la consulta del usuario.")
+                logger.error("Código de estado: %s", response.status_code)
+        except requests.exceptions.RequestException as err:
+            print(f"Error de solicitud: {err}")
+        except json.JSONDecodeError as err:
+            print(f"Error al decodificar JSON: {err}")
 
     except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as error:
         logger.error(
