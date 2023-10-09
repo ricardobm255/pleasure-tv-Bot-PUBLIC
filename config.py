d7629b0559b08ec905b660e3e6751178ac34f958
Ricardo Borrego <101906700+ricardobm255@users.noreply.github.com>
2023-10-09 00:19:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Configuracion de acceso y constantes"""
 
-BOT_TOKEN = "6666929357:AAFk2Wd7K13VPV8P-KB8X1A0Uu9pa46AENc" #  Bot token API
+BOT_TOKEN = "YOUR_BOT_TOKEN" #  Bot token API
 
-API_KEY = "7c35175bd929415d243d6bf2e1a6bb02" # API key (https://www.themoviedb.org/ )
+API_KEY = "YOUR_API_KEY" # API key (https://www.themoviedb.org/ )
 
 NUM_CHARACTER = 700
