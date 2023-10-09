c8a41a131009e7d0f17cc160631138428f8809da
Ricardo Borrego <101906700+ricardobm255@users.noreply.github.com>
2023-10-09 00:20:52 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 """Configuracion de acceso y constantes"""
 
-BOT_TOKEN = "6666929357:AAFk2Wd7K13VPV8P-KB8X1A0Uu9pa46AENc" #  Bot token API
+BOT_TOKEN = "YOUR_BOT_TOKEN" #  Bot token API
 
-API_KEY = "7c35175bd929415d243d6bf2e1a6bb02" # API key (https://www.themoviedb.org/ )
+API_KEY = "YOUR_API_TOKEN" # API key (https://www.themoviedb.org/ )
 
 NUM_CHARACTER = 600
 
-ADMIN_ID = (717325553,)
+ADMIN_ID = (YOUR ID OF TELEGRAM,)
